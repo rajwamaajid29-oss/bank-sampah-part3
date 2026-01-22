199538ab2c7a3419ff4ac128a1389a3ca6583226
rajwamaajid29-oss <rajwamaajid29@gmail.com>
2026-01-22 14:26:08 +0700

    Add files via upload

---
+++ database/migrations/2026_01_21_101308_create_type_trashes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('type_trashes', function (Blueprint $table) {
+            $table->id();
+            $table->string('types_names');
+            $table->integer('price_per_gram');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('type_trashes');
+    }
+};
